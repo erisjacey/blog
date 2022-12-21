168360a02830b2d4e8dff161849db45bd490bb8c
erisjacey-DESKTOP <eris_jacey@hotmail.com>
2022-12-21 14:37:32 +0800

    Enable map pan and zoom

--- components/mapchart.js
+++ components/mapchart.js
@@ -1,18 +1,46 @@
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import { 
+  ComposableMap, 
+  Geographies, 
+  Geography, 
+  ZoomableGroup
+} from 'react-simple-maps';
 
 const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries-sans-antarctica.json"
 
-const MapChart = () => {
+const MapChart = ({ setTooltipContent }) => {
   return (
-    <ComposableMap>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
+    <ComposableMap projection="geoEqualEarth">
+      <ZoomableGroup center={[0, 0]} zoom={1}>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography 
+                key={geo.rsmKey} 
+                geography={geo}
+                onMouseEnter={() => {
+                  setTooltipContent(`${geo.properties.name}`);
+                }}
+                onMouseLeave={() => {
+                  setTooltipContent('');
+                }}
+                style={{
+                  default: {
+                    fill: "#EEE",
+                  },
+                  hover: {
+                    fill: "#F53",
+                  },
+                  pressed: {
+                    fill: "#E42",
+                  },
+                }}
+                stroke="#000000"
+               />
+            ))
+          }
+        </Geographies>
+      </ZoomableGroup>
     </ComposableMap>
   )
 };
